6d8cec91f883b6892db090c27ae85446dd580210
zcookiez <annie9434@gamil.com>
2026-07-23 20:41:35 +0900

    fix: DB/JWT 설정 추가 및 Resolver 등록을 위한 WebConfig 생성

--- src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
+++ src/main/java/org/example/expert/config/AuthUserArgumentResolver.java
@@ -9,9 +9,11 @@ import org.springframework.core.MethodParameter;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+@Component
 public class AuthUserArgumentResolver implements HandlerMethodArgumentResolver {
 
     @Override
